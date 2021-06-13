191cbc8c6064339da7d65f7a9598e479c476e518
ke6ch <19749261+ke6ch@users.noreply.github.com>
2021-06-13 15:57:00 +0900

    Add tests

--- watson/tests.py
+++ watson/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.urls import resolve
 
-# Create your tests here.
+
+class TestUrls(TestCase):
+    def test_chat_url(self):
+        path = reverse("chat")
+        assert resolve(path).view_name == "chat"
